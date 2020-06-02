01fa6493adb724f970a1972946f3dcb0c1a1f259
Jason Peng <yipeng1@outlook.com>
2020-06-01 20:09:47 -0700

    shwoing vs code git hub

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
+
+//set up git hub
 function App() {
   return (
     <div className="App">
